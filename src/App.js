47dcf6bdcab05608d75204812dc25393332f7602
gregsnewby <gregnewby@Gregs-MacBook-Air.local>
2020-02-22 12:50:34 -0600

    Added Home and NotFound components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./Components/Layout/Navbar";
-import Users from "./Components/Users/Users";
-import Search from "./Components/Users/Search";
 import Alert from "./Components/Layout/Alert";
 import About from "./Components/Pages/About";
+import Home from "./Components/Pages/Home";
+import NotFound from "./Components/Pages/NotFound";
 import User from "./Components/Users/User";
-import "./App.css";
 
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
+
+import "./App.css";
 
 const App = () => {
 	return (
@@ -21,18 +22,10 @@ const App = () => {
 						<div className="container">
 							<Alert />
 							<Switch>
-								<Route
-									exact
-									path="/"
-									render={props => (
-										<Fragment>
-											<Search />
-											<Users />
-										</Fragment>
-									)}
-								/>
+								<Route exact path="/" component={Home} />
 								<Route exact path="/about" component={About} />
 								<Route exact path="/user/:login" component={User} />
+								<Route component={NotFound} />
 							</Switch>
 						</div>
 					</div>
